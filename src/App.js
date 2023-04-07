ea574081c7e4e663bd586b33e144d4b226c8ca1e
ThanuraSilva <thanurasilva1989@gmail.com>
2023-04-07 20:16:42 +0530

    Signin create

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { DataForm } from './components/dataform/DataForm';
 import { Nav } from './components/nav/Nav';
 import { Footer } from './components/footer/Footer';
 import SignUp from './components/signup/Signup';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
-     <SignUp/>
+     <Login/>
      <Footer/>
     </div>
   );
